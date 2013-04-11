8c8b0ff344baefde88f1c8ddb69e6bf8ccdb672c
Carl Meyer <carl@oddbird.net>
2013-04-10 18:10:47 -0600

    Add dotfiles installer for OS X.

---
+++ osx/install.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+if [ ! -f "$HOME/dot/osx/install.sh" ]; then
+  echo "must be cloned at ~/dot/"
+  exit 1
+fi
+
+for fn in gitconfig; do
+  src="$HOME/dot/${fn}"
+  dest="$HOME/.${fn}"
+  if [ -e $dest ]; then
+    echo "skipping $fn; already exists in homedir"
+  else
+    echo "linking $fn"
+    ln -s $src $dest
+  fi
+done
+
