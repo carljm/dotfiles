8d4885995f13c1a926c5c9fc92da509721f46d07
Carl Meyer <carl@oddbird.net>
2014-01-16 10:47:49 -0700

    Treat .j2 extension as a Jinja2 template also.

--- emacs.d/init-jinja.el
+++ emacs.d/init-jinja.el
@@ -1,5 +1,6 @@
 (add-to-list 'load-path "~/dot/emacs.d/jinja2-mode")
 (require 'jinja2-mode)
 (add-to-list 'auto-mode-alist '("\\.jinja2\\'" . jinja2-mode))
+(add-to-list 'auto-mode-alist '("\\.j2\\'" . jinja2-mode))
 
 (provide 'init-jinja)
