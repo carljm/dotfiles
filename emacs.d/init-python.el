6760117a41178c5f8912320c25a17998818df6a1
Carl Meyer <carl@oddbird.net>
2014-03-03 18:16:55 -0700

    Remove ropemacs.

--- emacs.d/init-python.el
+++ emacs.d/init-python.el
@@ -1,6 +1,5 @@
 (defun cjm-python-mode-hook ()
   (column-marker-1 80)
-  (ropemacs-mode)
   (define-key python-mode-map "" (quote newline-and-indent)))
 
 ; requires pyflakes
@@ -42,14 +41,5 @@ makes)."
     (list "pyflakes" (list local-file))))
 
 (add-hook 'find-file-hook 'flymake-find-file-hook)
-
-; requires pymacs, rope, ropemode, ropemacs
-;  sudo apt-get install pymacs
-;  sudo pip install rope ropemode https://bitbucket.org/agr/ropemacs/get/7e2e181c94df.tar.bz2
-(defun load-rope ()
-  (require 'pymacs)
-  (setq ropemacs-enable-shortcuts nil)
-  (setq ropemacs-local-prefix "C-c C-p")
-  (pymacs-load "ropemacs" "rope-"))
 
 (provide 'init-python)
