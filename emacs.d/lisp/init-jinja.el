9ccc24752816362bba51f7d9f146fbfa2ee4b9c1
Carl Meyer <carl@oddbird.net>
2016-04-29 12:35:04 -0600

    Enter jinja2-mode for .njk files.

--- emacs.d/lisp/init-jinja.el
+++ emacs.d/lisp/init-jinja.el
@@ -1,5 +1,6 @@
 (require 'jinja2-mode)
 (add-to-list 'auto-mode-alist '("\\.jinja2\\'" . jinja2-mode))
 (add-to-list 'auto-mode-alist '("\\.j2\\'" . jinja2-mode))
+(add-to-list 'auto-mode-alist '("\\.njk\\'" . jinja2-mode))
 
 (provide 'init-jinja)
